999df1995706154b1015e2f3d24c7535e0e657ea
roberto <rvneto.rs@gmail.com>
2024-06-15 19:46:59 -0300

    Commit inicial

---
+++ src/main/java/com/bank/pinnacle/core/db/PinnacleBankCoreDbApplication.java
@@ -0,0 +1,13 @@
+package com.bank.pinnacle.core.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinnacleBankCoreDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinnacleBankCoreDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/pinnacle/core/db/PinnacleBankCoreDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.pinnacle.core.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinnacleBankCoreDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
